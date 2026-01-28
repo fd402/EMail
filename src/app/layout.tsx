ad63391b54cc99430f087887e8806d7000815612
fd402 <fdvancek@gmail.com>
2026-01-28 22:13:28 +0100

    V5

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -36,6 +37,7 @@ export default function RootLayout({
       >
         <AuthProvider>
           <SubscriptionProvider>{children}</SubscriptionProvider>
+          <Analytics />
         </AuthProvider>
       </body>
     </html>
